78477a1bde8c11ff7a653d61013dfe9d85a8b592
Usman Anwar <uanwar88@gmail.com>
2015-03-30 13:47:01 -0500

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  resources :categories do
+    resources :users
+  end
+
+  resources :users
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
