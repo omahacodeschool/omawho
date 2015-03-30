fb2270778baa85c42d63a3c92e0ea16d5703589d
Usman Anwar <uanwar88@gmail.com>
2015-03-30 11:56:31 -0500

    Tried to fix category and user models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :categories_users
   has_many :users, through: :categories_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  belongs_to :category, through: :categories_users
+  has_one :categories_users
+  has_one :category, through: :categories_users
 end
