2e8821c402e4a750457d9b6a737695d0047426b5
Usman Anwar <uanwar88@gmail.com>
2015-04-01 10:31:33 -0500

    Built UserMailer and ActionMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,14 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.reset_password_email.subject
   #
+
+  def welcome_mail(user)
+    default from: 'elpresidente@omawho.com'
+    @user = user
+    @url = "http://www.omawho.com/login"
+    mail(to: @user.email, subject: 'Welcome to Omawho')
+  end
+
   def reset_password_email(user)
     @user = User.find user.id
     @url  = edit_password_reset_url(@user.reset_password_token)
