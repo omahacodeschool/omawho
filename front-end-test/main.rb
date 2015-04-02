60fd4c31b95e01ad3d6da826141b6e019cb45ef9
Derek George <derek68038@gmail.com>
2015-04-02 12:03:34 -0500

    Added edit user, forgot password, login, and new password pages.

--- front-end-test/main.rb
+++ front-end-test/main.rb
@@ -8,6 +8,22 @@ get "/new" do
   erb :new_user, :layout => :'layout1'
 end
 
+get "/edit" do
+  erb :edit_user, :layout => :'layout1'
+end
+
 get "/profile" do
   erb :profile, :layout => :'layout1'
+end
+
+get "/login" do
+  erb :login, :layout => :'layout1'
+end
+
+get "/forgot" do
+  erb :forgot_password, :layout => :'layout1'
+end
+
+get "/sorcery" do
+  erb :new_password, :layout => :'layout1'
 end
