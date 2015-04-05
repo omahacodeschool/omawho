283d4ff28c2afe4612d03053a78dfe6913f252c1
Usman Anwar <uanwar88@gmail.com>
2015-04-05 14:10:25 -0500

    Configured mailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,15 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 465,
+  :domain               => "gmail.lv",
+  :user_name            => "uanwar88@gmail.com",
+  :password             => ENV['email_password'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
 
   config.action_mailer.default_url_options = { :host => 'localhost' }
   # In the development environment your application's code is reloaded on
