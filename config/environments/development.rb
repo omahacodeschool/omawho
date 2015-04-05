a7882d8e2cd982b66bfa602e3fe8f36c106d4c04
Usman Anwar <uanwar88@gmail.com>
2015-04-05 13:58:25 -0500

    Development.rb config changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default_url_options = { :host => 'localhost' }
   # In the development environment your application's code is reloaded on
