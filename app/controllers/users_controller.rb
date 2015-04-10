e8310afc143a0aa5bb1e9198578f1bf792a09fe7
Usman Anwar <uanwar88@gmail.com>
2015-04-10 09:53:16 -0500

    Edited index action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,7 @@ class UsersController < ApplicationController
 
   def index
     @categories = Category.all
-    @users = User.joins(:category).select("users.username, users.first_name, users.last_name,
-    users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
+    @users = User.joins(:category).select("users.username, users.first_name, users.last_name, users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
     order("RANDOM()").page(params[:page])
   end
 
