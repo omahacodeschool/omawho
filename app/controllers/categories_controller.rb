bdb6df52a158e47625e3817c7d2c051a2bb6b049
Derek George <derek68038@gmail.com>
2015-04-10 14:25:23 -0500

    Fixed colored bar under categories when hovered.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -14,6 +14,7 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.all
     @highlighted = Category.find(params[:id]).short_category_name
+    @user = User.new
     if params[:page]
       @users = User.joins(:category).select("users.username, users.first_name, users.last_name, users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
       order("RANDOM()").where(category_id: params[:id]).page(params[:page])
