17aa4f7e88faa66cc3df82c7b1db079cf0fa41e9
Usman Anwar <uanwar88@gmail.com>
2015-04-10 10:50:20 -0500

    Added ID to users query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,14 @@ class UsersController < ApplicationController
 
   def index
     @categories = Category.all
-    @users = User.joins(:category).select("users.username, users.first_name, users.last_name, users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
+    @users = User.joins(:category).select("users.id, users.username, users.first_name, users.last_name, users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
     order("RANDOM()").page(1)
   end
 
   def page
-    @users = User.joins(:category).select("users.username, users.first_name, users.last_name, users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
-    order("RANDOM()").page(1).to_json
+    @users = User.joins(:category).select("users.id, users.username, users.first_name, users.last_name, users.avatar, categories.name AS category_name, setseed(#{session[:seed]})").
+    order("RANDOM()").page(1)
+    render text: @users.to_json
   end
 
   def create
