3d42f85a35689663155a9c65dfc7f79ebc1d0987
Sumeet Jain <sumeet@sumeetjain.com>
2015-04-16 16:16:20 -0500

    Change mail host to omawho.com. TODO - Set using ENV var.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'omawho-v2-staging.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'omawho.com' }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
