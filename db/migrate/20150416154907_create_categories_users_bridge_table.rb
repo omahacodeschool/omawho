9c0669a8274d81edd1b1b7f98c9a101807b6eff1
Sumeet Jain <sumeet@sumeetjain.com>
2015-04-16 13:11:38 -0500

    Make the migrations a bit more defensive.

--- db/migrate/20150416154907_create_categories_users_bridge_table.rb
+++ db/migrate/20150416154907_create_categories_users_bridge_table.rb
@@ -1,8 +1,10 @@
 class CreateCategoriesUsersBridgeTable < ActiveRecord::Migration
   def change
-    create_table :categories_users do |t|
-      t.integer :category_id, index: true
-      t.integer :user_id, index: true
+    if !table_exists?("categories_users")
+      create_table :categories_users do |t|
+        t.integer :category_id, index: true
+        t.integer :user_id, index: true
+      end
     end
   end
 end
