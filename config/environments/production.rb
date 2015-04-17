c3af3bec6b67e8e7beabd61c30d9cc2093563ce7
Derek George <derek68038@gmail.com>
2015-04-17 09:59:20 -0500

    Added env variable.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => 'omawho.com' }
+  config.action_mailer.default_url_options = { :host => ENV['MAIL_HOST_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
